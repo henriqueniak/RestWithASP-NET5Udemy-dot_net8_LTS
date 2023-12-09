b0b4baef1c00f2d0501ae7b809de9778947151bf
Henrique Ratuchiniak <henriqueniak.web@gmail.com>
2023-12-09 18:05:51 -0300

    first commit

---
+++ 16_RestWithASPNETUdemy_PATHVerb/RestWithASPNETErudio/RestWithASPNETErudio/Business/IPersonBusiness.cs
@@ -0,0 +1,14 @@
+﻿using RestWithASPNETErudio.Data.VO;
+
+namespace RestWithASPNETErudio.Business
+{
+    public interface IPersonBusiness
+    {
+        PersonVO Create(PersonVO person);
+        PersonVO FindByID(long id);
+        List<PersonVO> FindAll();
+        PersonVO Update(PersonVO person);
+        PersonVO Disable(long id);
+        void Delete(long id);
+    }
+}
